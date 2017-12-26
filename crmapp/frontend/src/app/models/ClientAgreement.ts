a400ecac1070778841bc4f3fea2e9df25a6a6911
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2017-12-26 14:06:36 +0200

    UI: new property 'documents'

--- crmapp/frontend/src/app/models/ClientAgreement.ts
+++ crmapp/frontend/src/app/models/ClientAgreement.ts
@@ -1,7 +1,10 @@
+import { Document } from "./Document";
+
 export class ClientAgreement {
     public id?: number;
     public clientId?: number;
     public number?: string;
     public dateStart?: string;
+    public documents?: Document[];
     public comment?: string;
 }
