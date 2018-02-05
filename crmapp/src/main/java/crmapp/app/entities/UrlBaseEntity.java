0db48290cb90423fbcd265da63985c1c9cc97a92
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-05 10:45:32 +0200

    deleted unused import

--- crmapp/src/main/java/crmapp/app/entities/UrlBaseEntity.java
+++ crmapp/src/main/java/crmapp/app/entities/UrlBaseEntity.java
@@ -2,8 +2,6 @@ package crmapp.app.entities;
 
 import javax.persistence.MappedSuperclass;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 @MappedSuperclass
 public class UrlBaseEntity extends BaseEntity {
 	
