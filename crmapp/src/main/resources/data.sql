682ce675e506e7f0eda6e511c130bec3e3baac6e
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-06 14:39:59 +0200

    added data for category.icon, fixed routerLink

--- crmapp/src/main/resources/data.sql
+++ crmapp/src/main/resources/data.sql
@@ -5,15 +5,15 @@ USE `crmapp`;
 SET FOREIGN_KEY_CHECKS=0;
 
 TRUNCATE `category`;
-INSERT INTO `category` (`id`, `label`, `router_link`, `comment`, `optlock`)
+INSERT INTO `category` (`id`, `label`, `icon`, `router_link`, `comment`, `optlock`)
 VALUES 
-  (1, 'Клиенты', 'clients', '', 0),
-  (2, 'Договорные отношения', 'agreements', '', 0),
-  (3, 'Документооборот', 'documents', '', 0),
-  (4, 'Сотрудники', 'employees', '', 0),
-  (5, 'Отпуски', 'vacations', '', 0),
-  (6, 'Больничные листы', 'sicklists', '', 0),
-  (7, 'Исходящий номер', 'outputs', '', 0);
+  (1, 'Клиенты', 'fa-users', '/clients', '', 0),
+  (2, 'Договорные отношения', 'fa-file-o', '/agreements', '', 0),
+  (3, 'Документооборот', 'fa-file-o', '/documents', '', 0),
+  (4, 'Сотрудники', 'fa-users', '/employees', '', 0),
+  (5, 'Отпуски', 'fa-file-o', '/vacations', '', 0),
+  (6, 'Больничные листы', 'fa-file-o', '/sicklists', '', 0),
+  (7, 'Исходящий номер', 'fa-file-o', '/outputs', '', 0);
 
 TRUNCATE `client`;
 INSERT INTO `client` (`id`, `alias`, `title`, `edrpou`, `inn`, `vat_certificate`, `optlock`)
