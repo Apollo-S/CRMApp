a92fbd288369d874ed46b634fbee90aef652a258
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-09 01:46:36 +0200

    UI: cleaned unused code from dashboard.ts

--- crmapp/frontend/src/app/components/dashboard/dashboard.component.ts
+++ crmapp/frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Category } from '../../models/Category';
 import { CategoryService } from '../../services/category.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Client } from '../../models/Client';
-import { ClientService } from '../../services/client.service';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -12,30 +9,19 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  categories: Category[];
   items: MenuItem[];
-  clients: Client[];
   
   constructor(private service: CategoryService,
-              private clientService: ClientService,
               private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.getCategories();
-    this.getClients();
   }
 
   private getCategories() {
     this.service.getCategories()
-      .subscribe(categories => this.items = categories);
+      .subscribe(items => this.items = items);
   }
 
-  private getClients() {
-    this.clientService.getClients()
-      .subscribe(clients => this.clients = clients);
-  }
-
-  
- 
 }
