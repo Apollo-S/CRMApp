6208e0c9eccaf8e7c64727671f916c675b6a336d
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-10 11:47:16 +0200

    UI: fixed editing client, not updates parent title, validation needed

--- crmapp/frontend/src/app/components/client/edit-client/edit-client.component.ts
+++ crmapp/frontend/src/app/components/client/edit-client/edit-client.component.ts
@@ -1,34 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Client } from '../../../models/Client';
 import { ClientService } from '../../../services/client.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-client',
   templateUrl: './edit-client.component.html',
   styleUrls: ['./edit-client.component.css']
 })
-export class EditClientComponent implements OnInit {
-  client: Client = {};
-  id: number;
+export class EditClientComponent implements OnInit, OnDestroy {
+  private _propertySubscribtion: Subscription;
 
-  constructor(private clientService: ClientService,
+  userform: FormGroup;
+  submitted: boolean;
+
+  client: Client = {};
+  clientId: number;
+
+  constructor(private fb: FormBuilder,
+              private clientService: ClientService,
               private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.id = parseInt(this.route.snapshot.params['id']);
-    this.clientService.getClientById(this.id)
+    this._propertySubscribtion = this.clientService.property$
+      .subscribe(p => {
+        this.clientId = p;
+      });
+    this.clientService.getClientById(this.clientId)
       .subscribe(client => this.client = client);
+    // this.userform = this.fb.group(
+    //   {
+    //     'title': new FormControl('', Validators.required),
+    //     'alias': new FormControl('', Validators.required),
+
+    //   });
   }
 
   onSubmit() {
     this.update();
-    location.reload();
-    }
+    this.ngOnInit();
+  }
 
-    private update(): void {
-      this.clientService.updateClient(this.client);
-    }
+  ngOnDestroy() {
+    this._propertySubscribtion.unsubscribe();
+  }
+
+  private update(): void {
+    this.clientService.updateClient(this.client)
+      .subscribe(response => this.client = response)
+  }
 
 }
