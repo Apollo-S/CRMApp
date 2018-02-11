01a2906f2501698cf37fb6d993e0d3df643911f7
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-11 11:07:27 +0200

    UI: changed data type of field 'dateStart'

--- crmapp/frontend/src/app/models/ClientAddress.ts
+++ crmapp/frontend/src/app/models/ClientAddress.ts
@@ -2,6 +2,6 @@ export class ClientAddress {
     public id?: number;
     public clientId?: number;
     public presentation?: string;
-    public dateStart?: string;
+    public dateStart?: Date;
     
 }
