813d3c45b75c88534647a8086c0ef861aec5318b
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-12 01:53:26 +0200

    UI: rewrote add address form, add delay for routing

--- crmapp/frontend/src/app/components/client/client-details/client-details-addresses-tab/add-address/add-address.component.ts
+++ crmapp/frontend/src/app/components/client/client-details/client-details-addresses-tab/add-address/add-address.component.ts
@@ -3,6 +3,8 @@ import { ClientService } from '../../../../../services/client.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ClientAddress } from '../../../../../models/ClientAddress';
 import { Subscription } from 'rxjs';
+import { ConfirmationService, Message } from 'primeng/api';
+import { MessageService } from 'primeng/components/common/messageservice';
 
 @Component({
   selector: 'app-add-address',
@@ -11,12 +13,14 @@ import { Subscription } from 'rxjs';
 })
 export class AddAddressComponent implements OnInit, OnDestroy {
   private _propertySubscribtion: Subscription;
+  msgs: Message[] = [];
   address: ClientAddress = {};
   clientId: number;  
 
   constructor(private service: ClientService, 
               private router: Router,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private messageService: MessageService) { }
 
   ngOnInit() {
     this._propertySubscribtion = this.service.property$
@@ -37,9 +41,15 @@ export class AddAddressComponent implements OnInit, OnDestroy {
   private save(): void {
     this.service.addAddress(this.address, this.clientId)
       .subscribe(response => {
-          this.router.navigate(['/clients', this.clientId, 'addresses']);
-        }
-      );
+          this.messageService.add({severity:'success', summary:'Service Message', detail:'Via MessageService'});
+          this.goBackToAddresses();
+      });
+  }
+
+  private goBackToAddresses() {
+    setTimeout((router) => {
+      this.router.navigate(['/clients', this.clientId, 'addresses']);
+    }, 1500);
   }
 
 }
