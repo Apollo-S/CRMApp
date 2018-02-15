645b46969818508634c48a42d20ba1a49d01d62e
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-15 16:04:23 +0200

    UI: utilService, deleted unused imports, fixing in url

--- crmapp/frontend/src/app/components/client/client-details/client-details-accounts-tab/add-account/add-account.component.ts
+++ crmapp/frontend/src/app/components/client/client-details/client-details-accounts-tab/add-account/add-account.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ClientAccount } from '../../../../../models/ClientAccount';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { UtilService } from '../../../../../services/util.service';
 import { ClientService } from '../../../../../services/client.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ClientAccount } from '../../../../../models/ClientAccount';
+import { Client } from '../../../../../models/Client';
 import { Message } from 'primeng/api';
 
 @Component({
@@ -13,24 +15,22 @@ import { Message } from 'primeng/api';
 export class AddAccountComponent implements OnInit, OnDestroy {
   private _propertySubscribtion: Subscription;
   msgs: Message[] = [];
+  client: Client = {};
   account: ClientAccount = {};
-  clientId: number;  
   years: string;
 
   constructor(private service: ClientService, 
-              private router: Router,
-              private route: ActivatedRoute) { }
+              private utilService: UtilService,
+              private router: Router) { }
 
   ngOnInit() {
     this._propertySubscribtion = this.service.property$
-    .subscribe(p => {
-      this.clientId = p;
-      }
+      .subscribe(
+        p => this.client = p
     );
-    let currentYear = new Date().getFullYear();
-    this.years = ((currentYear - 5).toString()) + ':' + currentYear.toString();
+    this.years = this.utilService.getYears();
   }
-
+  
   onSubmit() {
     this.save();
     this.goBackToAccounts();
@@ -42,16 +42,19 @@ export class AddAccountComponent implements OnInit, OnDestroy {
 
   private save(): void {
     let msg  = '';
-    this.service.addAccount(this.account, this.clientId)
-      .subscribe(response => {
-        msg = 'Счет успешно добавлен (ID=' + response.id + ')';
+    this.service.addAccount(this.account, this.client)
+      .subscribe(
+        response => {
+          msg = 'Счет для ' + this.client.alias + ' успешно добавлен (ID=' + response.id + ')';
         this.msgs = [{severity:'success', summary:'Успешно', detail: msg}];
-      });
+        }
+      );
   }
 
   private goBackToAccounts() {
-    setTimeout((router) => {
-      this.router.navigate(['/clients', this.clientId, 'accounts']);
+    setTimeout(
+      (router) => {
+        this.router.navigate([this.client.url, 'accounts']);
     }, 1500);
   } 
 
