2d6595c9f9f902b2d4a10522ab917b6e9c08fa77
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-16 17:06:29 +0200

    UI: utilService, messages, confirmDeleting etc.

--- crmapp/frontend/src/app/components/client/client-details/client-details-directors-tab/edit-director/edit-director.component.ts
+++ crmapp/frontend/src/app/components/client/client-details/client-details-directors-tab/edit-director/edit-director.component.ts
@@ -1,88 +1,115 @@
-import { Component, OnInit } from '@angular/core';
-import { ClientDirector } from '../../../../../models/ClientDirector';
-import { Post } from '../../../../../models/Post';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ClientService } from '../../../../../services/client.service';
 import { PostService } from '../../../../../services/post.service';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UtilService } from '../../../../../services/util.service';
+import { Client } from '../../../../../models/Client';
+import { Post } from '../../../../../models/Post';
+import { ClientDirector } from '../../../../../models/ClientDirector';
+import { ConfirmationService, Message } from 'primeng/api';
 
 @Component({
   selector: 'app-edit-director',
   templateUrl: './edit-director.component.html',
   styleUrls: ['./edit-director.component.css']
 })
-export class EditDirectorComponent implements OnInit {
+export class EditDirectorComponent implements OnInit, OnDestroy {
   private _propertySubscribtion: Subscription;
+  msgs: Message[] = [];
+  client: Client = {};
   director: ClientDirector = {};
-  directorId: number;
-  posts: Post[];
-  clientId: number; 
+  posts: Post[] = [];
+  years: string;
   
   constructor(private service: ClientService, 
               private postService: PostService,
+              private utilService: UtilService,
               private router: Router,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private confirmationService: ConfirmationService) { }
 
   ngOnInit() {
-    this.route.params
-    .subscribe(
-      (params: Params) => {
-        this.directorId = +params['id'];
-        }
-      )
+    let directorId: number;
     this._propertySubscribtion = this.service.property$
-      .subscribe(p => {
-        this.clientId = p;
+      .subscribe(
+        p => this.client = p
+      );
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          directorId = +params['id'];
+          this.getDirectorById(directorId, this.client);
         }
       );
-    this.getDirectorById(this.directorId, this.clientId);
     this.getPosts();
+    this.years = this.utilService.getYears();
   }
-
-  onSubmit() {
-    this.update();
-  }
-
-  delete() {
-    if (confirm("Удалить текущего директора?")) {
-      this.service.deleteDirector(this.directorId, this.clientId)
-        .subscribe(response => {
-          this.goBackToDirectors();
-        });
-    }; 
-  }  
 
   ngOnDestroy(): void {
     this._propertySubscribtion.unsubscribe();
   }
 
-  private update(): void {
-    this.service.updateDirector(this.director, this.clientId)
-      .subscribe(response => {
-        this.goBackToDirectors();
-      })
+  onSubmit() {
+    this.update();
+    this.goBackToDirectors();
   }
 
-  private getDirectorById(directorId: number, clientId: number) {
-    this.service.getDirectorById(directorId, clientId)
-      .subscribe(director => {
-        this.director = director;
-      });
+  confirmDeleting() {
+    this.confirmationService.confirm({
+      message: 'Действительно удалить руководителя?',
+      header: 'Удаление адреса',
+      icon: 'fa fa-trash',
+      accept: () => {
+        let msg  = 'Руководитель успешно удален (ID=' + this.director.id + ')';
+        this.delete(msg);
+        this.goBackToDirectors();
+      },
+      reject: () => {}
+    });
   }
-  
-  private goBackToDirectors() {
-    this.router.navigate(['/clients', this.clientId, 'directors']);
+
+  private delete(msg: string) {
+    this.service.deleteDirector(this.director.id, this.client)
+      .subscribe(
+        response => {
+          this.msgs = [{severity:'success', summary:'Успешно', detail: msg}];
+        }
+      );
+  }
+
+  private update(): void {
+    this.service.updateDirector(this.director, this.client)
+      .subscribe(
+        response => {
+          let msg = 'Руководитель для ' + this.client.alias +  ' успешно обновлен (ID=' + response.id + ')';
+          this.msgs = [{severity:'success', summary:'Успешно', detail: msg}];
+        }
+      );
+  }
+
+  private getDirectorById(directorId: number, client: Client) {
+    this.service.getDirectorById(directorId, client)
+      .subscribe(
+        director => {
+          this.director = director;
+          this.director.dateStart = new Date(this.director.dateStart);
+        }
+      );
   }
 
   private getPosts() {
     this.postService.getPosts()
-      .subscribe(post => {
-        this.posts = post;
-      })
+      .subscribe(
+        post => this.posts = post
+      );
   }
-
-  compareFn(c1: Post, c2: Post): boolean {
-    return c1 && c2 ? c1.id === c2.id : c1 === c2;
-  }
+  
+  private goBackToDirectors() {
+    setTimeout(
+      (router) => {
+        this.router.navigate([this.client.url, 'directors']);
+      }, 1500);
+  } 
 
 }
