119bc143ba0da5370c7bc96f2e9a1221ab837212
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-17 16:39:53 +0200

    UI: huge changes in edit client form, validation is needed

--- crmapp/frontend/src/app/components/client/edit-client/edit-client.component.ts
+++ crmapp/frontend/src/app/components/client/edit-client/edit-client.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Client } from '../../../models/Client';
-import { ClientService } from '../../../services/client.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { Subscription } from 'rxjs';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ClientService } from '../../../services/client.service';
+import { Client } from '../../../models/Client';
+import { Message } from 'primeng/api';
 
 @Component({
   selector: 'app-edit-client',
@@ -12,12 +13,11 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 })
 export class EditClientComponent implements OnInit, OnDestroy {
   private _propertySubscribtion: Subscription;
-
+  msgs: Message[] = [];
   userform: FormGroup;
   submitted: boolean;
 
   client: Client = {};
-  clientId: number;
 
   constructor(private fb: FormBuilder,
               private clientService: ClientService,
@@ -26,31 +26,41 @@ export class EditClientComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this._propertySubscribtion = this.clientService.property$
-      .subscribe(p => {
-        this.clientId = p;
-      });
-    this.clientService.getClientById(this.clientId)
-      .subscribe(client => this.client = client);
+      .subscribe(
+        p => this.client = p
+      );
     // this.userform = this.fb.group(
     //   {
     //     'title': new FormControl('', Validators.required),
     //     'alias': new FormControl('', Validators.required),
-
     //   });
   }
-
-  onSubmit() {
-    this.update();
-    this.ngOnInit();
-  }
-
+  
   ngOnDestroy() {
     this._propertySubscribtion.unsubscribe();
+  }
+  
+  onSubmit() {
+    this.update();
+    this.goBackToClient(1500);
   }
 
   private update(): void {
     this.clientService.updateClient(this.client)
-      .subscribe(response => this.client = response)
+      .subscribe(
+        response => {
+          this.client = response;
+          let msg = 'Клиент ' + this.client.alias +  ' успешно обновлен (ID=' + response.id + ')';
+          this.msgs = [{severity:'success', summary:'Успешно', detail: msg}];
+        }
+      );
   }
+
+  private goBackToClient(timeMillis: number) {
+    setTimeout(
+      (router) => {
+        this.router.navigate([this.client.url]);
+      }, timeMillis);
+  } 
 
 }
