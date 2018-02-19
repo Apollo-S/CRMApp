bed763e960e58ba43070d557e93ff4a8339f874e
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-19 10:10:17 +0200

    added properties in JsonIgnoreProperties for ignoring

--- crmapp/src/main/java/crmapp/app/entities/Client.java
+++ crmapp/src/main/java/crmapp/app/entities/Client.java
@@ -18,7 +18,8 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 @Table(name = "client")
 @JsonIgnoreProperties(ignoreUnknown = true, 
-	value = { "hibernateLazyInitializer", "handler" })
+	value = { "hibernateLazyInitializer", "handler",
+			"agreements", "addresses", "directors", "accounts" })
 public class Client extends UrlBaseEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
