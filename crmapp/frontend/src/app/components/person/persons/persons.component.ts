149bd908e9178a91546c17c2bfdf5e5cc6532ff5
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-25 02:38:02 +0200

    UI: presentation of persons page

--- crmapp/frontend/src/app/components/person/persons/persons.component.ts
+++ crmapp/frontend/src/app/components/person/persons/persons.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonService } from '../../../services/person.service';
+import { Person } from '../../../models/Person';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
   styleUrls: ['./persons.component.css']
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit {  
+  persons: Person[] = [];
+  columns: any[];
 
-  constructor() { }
+  constructor(private service: PersonService) { }
 
   ngOnInit() {
+    this.getPersons();
+    this.initColumns();
+  }
+
+  private getPersons(): any {
+    this.service.getPersons()
+      .subscribe(
+        persons => this.persons = persons
+      );
+  }
+
+  initColumns(): any {
+    this.columns = [
+      { field: 'surname', header: 'Фамилия' },
+      { field: 'firstname', header: 'Имя' },
+      { field: 'lastname', header: 'Отчество' }
+    ];
   }
 
 }
