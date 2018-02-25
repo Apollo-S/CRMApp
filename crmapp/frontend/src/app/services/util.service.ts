dd308f80d64761505fa79ed2ad87506b57c1f286
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-02-26 01:31:26 +0200

    UI: new method getCalendarLocalSet() (returning ru local)

--- crmapp/frontend/src/app/services/util.service.ts
+++ crmapp/frontend/src/app/services/util.service.ts
@@ -5,10 +5,22 @@ export class UtilService {
 
   constructor() { }
 
-  getYears(): string {
-    let countOfYears = 5;
+  getCalendarYears(countOfYears: number): string {
     let currentYear = new Date().getFullYear();
     return ((currentYear - countOfYears).toString()) + ':' + currentYear.toString();
+  }
+
+  getCalendarLocalSet(): any  {
+    return {
+      firstDayOfWeek: 1,
+      dayNames: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
+      dayNamesShort: ["Вос", "Пон", "Втор", "Ср", "Чет", "Пят", "Суб"],
+      dayNamesMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+      monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+      monthNamesShort: [ "Янв", "Фев", "Мар", "Апр", "Май", "Июн","Июл", "Авг", "Сен", "Окт", "Ноя", "Дек" ],
+      today: 'Сегодня',
+      clear: 'Очистить'
+    };
   }
 
 }
