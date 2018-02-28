8e06af22f5f5ca2f539774970d452b6063c7b337
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-03-01 01:59:42 +0200

    UI: changes in clients table, new elements
    + added loading icon while table is loading;
    + enabled rowHover;
    + added button with 'p-tieredMenu';
    + new methods in ts;
    * replaced class="text-center" with style="text-align:center";

--- crmapp/frontend/src/app/components/client/clients/clients.component.ts
+++ crmapp/frontend/src/app/components/client/clients/clients.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../../../services/client.service';
 import { Client } from '../../../models/Client';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-clients',
@@ -10,12 +12,19 @@ import { Client } from '../../../models/Client';
 export class ClientsComponent implements OnInit {
   clients: Client[];
   columns: any[];
+  loading: boolean;
+  items: MenuItem[];
   
-  constructor(private service: ClientService) { }
+  constructor(private service: ClientService,
+              private router: Router) { }
 
   ngOnInit() {
     this.getClients();
     this.initColumns();
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+    }, 500);
   } 
 
   private getClients(): any {
@@ -30,6 +39,19 @@ export class ClientsComponent implements OnInit {
       { field: 'alias', header: 'Наименование' },
       { field: 'title', header: 'Полное наименование' },
       { field: 'edrpou', header: 'ЕГРПОУ' }
+    ];
+  }
+
+  goToClient(url: string) {
+    this.router.navigate([url]);
+  }
+
+  initMenu(clientId: string) {
+    this.items = [
+      { label: 'Договоры', icon: 'fa-file-text-o', routerLink: [clientId, 'agreements'] },
+      { label: 'Адресы', icon: 'fa-building-o', routerLink: [clientId, 'addresses'] },
+      { label: 'Руководители', icon: 'fa-user-o', routerLink: [clientId, 'directors'] },
+      { label: 'Банк. реквизиты', icon: 'fa-bank', routerLink: [clientId, 'accounts'] }
     ];
   }
 
