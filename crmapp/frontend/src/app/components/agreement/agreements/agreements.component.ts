0c8341b38e6d7494245dda3f400f794b8de541cb
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-03-03 17:14:53 +0200

    UI: rewrote agreements html and ts

--- crmapp/frontend/src/app/components/agreement/agreements/agreements.component.ts
+++ crmapp/frontend/src/app/components/agreement/agreements/agreements.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AgreementService } from '../../../services/agreement.service';
 import { ClientAgreement } from '../../../models/ClientAgreement';
+import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agreements',
@@ -8,20 +10,46 @@ import { ClientAgreement } from '../../../models/ClientAgreement';
   styleUrls: ['./agreements.component.css']
 })
 export class AgreementsComponent implements OnInit {
+  agreements: ClientAgreement[] = [];
+  columns: any[];
+  loading: boolean;
+  items: MenuItem[];
 
-  agreements: ClientAgreement[];
-
-  constructor(private agreementService: AgreementService) { }
+  constructor(private service: AgreementService, 
+              private router: Router) { }
 
   ngOnInit() {
     this.getAgreements();
+    this.initColumns();
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+    }, 500);
   }
 
-  getAgreements() {
-    this.agreementService.getAgreements()
-      .subscribe(agreements => {
-        this.agreements = agreements
-      });
+  private getAgreements() {
+    this.service.getAgreements()
+      .subscribe(
+        agreements => this.agreements = agreements
+      );
+  }
+
+  private initColumns() {
+    this.columns = [
+      { field: 'clientAlias', header: 'Контрагент' },
+      { field: 'number', header: 'Номер' },
+      { field: "dateStart", header: 'Дата' }
+    ];
+  }
+
+  goToAgreement(url: string) {
+    this.router.navigate([url]);
+  }
+
+  initMenu(agreementId: string) {
+    this.items = [
+      { label: 'Связ. документы', icon: 'fa-file-text-o', routerLink: [agreementId, 'documents'] }
+    ];
   }
 
 }
