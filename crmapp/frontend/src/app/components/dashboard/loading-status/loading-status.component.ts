9bebc39bdda4583881f777b7f59cb044e155e9f4
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-03-09 21:58:26 +0200

    UI: removed loading circle to separate component

---
+++ crmapp/frontend/src/app/components/dashboard/loading-status/loading-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-status',
+  templateUrl: './loading-status.component.html',
+  styleUrls: ['./loading-status.component.css']
+})
+export class LoadingStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
