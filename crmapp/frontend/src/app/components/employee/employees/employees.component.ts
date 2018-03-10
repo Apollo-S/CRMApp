5d25da92931d14d5224e75f7f21934f3034b838b
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-03-10 14:28:37 +0200

    UI: replaced code of employee's view with 'app-dashboard-table' options

--- crmapp/frontend/src/app/components/employee/employees/employees.component.ts
+++ crmapp/frontend/src/app/components/employee/employees/employees.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { EmployeeService } from '../../../services/employee.service';
 import { Employee } from '../../../models/Employee';
 import { MenuItem } from 'primeng/api';
@@ -12,19 +11,14 @@ import { MenuItem } from 'primeng/api';
 export class EmployeesComponent implements OnInit {
   employees: Employee[] = [];
   columns: any[];
-  loading: boolean;
   items: MenuItem[];
   
-  constructor(private service: EmployeeService,
-              private router: Router) { }
+  constructor(private service: EmployeeService) { }
   
   ngOnInit() {
-    this.getEmployees();
     this.initColumns();
-    this.loading = true;
-    setTimeout(() => {
-      this.loading = false;
-    }, 500);
+    this.getEmployees();
+    this.initMenu();
   }
   
   private getEmployees(): any {
@@ -42,16 +36,12 @@ export class EmployeesComponent implements OnInit {
     ];
   }
 
-  goToEmployee(url: string) {
-    this.router.navigate([url]);
-  }
-
-  initMenu(employeeId: string) {
+  private initMenu() {
     this.items = [
-      { label: 'Договоры', icon: 'fa-file-text-o', routerLink: [employeeId, 'documents'] },
-      { label: 'Адресы', icon: 'fa-building-o', routerLink: [employeeId, 'addresses'] },
-      { label: 'Отпуск/Больн. листы', icon: 'fa-user-o', routerLink: [employeeId, 'vacations-sicks'] },
-      { label: 'Банк. реквизиты', icon: 'fa-bank', routerLink: [employeeId, 'accounts'] }
+      { label: 'Договоры', icon: 'fa-file-text-o', title: 'documents' },
+      { label: 'Адресы', icon: 'fa-building-o', title: 'addresses' },
+      { label: 'Отпуск/Больн. листы', icon: 'fa-user-o', title: 'vacations-sicks' },
+      { label: 'Банк. реквизиты', icon: 'fa-bank', title: 'accounts' }
     ];
   }
 
