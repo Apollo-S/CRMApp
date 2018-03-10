823e80c54a29023ba7a91e072a97b8d0fbf5588b
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-03-10 13:23:26 +0200

    UI: now clients and agreements views work through dashboard table
    * html has common code for generating views

--- crmapp/frontend/src/app/components/dashboard/dashboard-table/dashboard-table.component.ts
+++ crmapp/frontend/src/app/components/dashboard/dashboard-table/dashboard-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard-table',
@@ -16,13 +17,24 @@ export class DashboardTableComponent implements OnInit {
   @Input ('rowHover') rowHover: boolean; 
   @Input ('paginator') paginator: boolean; 
   @Input ('rows') rows: number; 
+  @Input ('loading') loading: boolean; 
+  @Input ('menuModel') menuModel: MenuItem[]; 
   constructor(private router: Router) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 700);
   }
 
   goToEntry(url: string) {
     this.router.navigate([url]);
+  }
+
+  initMenu(datasourceId: string) {
+    this.menuModel.forEach(
+      (menu) => menu.routerLink = [datasourceId, menu.title]
+    );
   }
 
 }
