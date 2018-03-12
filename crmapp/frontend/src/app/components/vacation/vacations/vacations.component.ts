aff048af7b72db7c2f56897f289ca2010bd60ac9
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-03-12 17:38:49 +0200

    UI: rewrote 'vacations' presentation (html+ts)

--- crmapp/frontend/src/app/components/vacation/vacations/vacations.component.ts
+++ crmapp/frontend/src/app/components/vacation/vacations/vacations.component.ts
@@ -1,25 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { VacationService } from '../../../services/vacation.service';
 import { Vacation } from '../../../models/Vacation';
+import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vacations',
   templateUrl: './vacations.component.html',
   styleUrls: ['./vacations.component.css']
 })
-
 export class VacationsComponent implements OnInit {
-  vacations: Array<Vacation> = [];
+  vacations: Vacation[] = [];
+  columns: any[];
+  items: MenuItem[];
 
-  constructor(private vacationService: VacationService) { }
+  constructor(private vacationService: VacationService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.initColumns();
     this.getVacations();
   }
 
-  private getVacations() {
+  private getVacations(): any {
     this.vacationService.getVacations()
-      .subscribe(vacations => this.vacations = vacations);
+      .subscribe(
+        vacations => this.vacations = vacations
+      );
+  }
+
+  private initColumns() {
+    this.columns = [
+      { field: 'employeeShortName', header: 'Сотрудник' },
+      { field: 'description', header: 'Описание' },
+      { field: 'dateStart', header: 'Период c' },
+      { field: 'dateFinal', header: 'Период по' }
+    ];
+  }
+
+  private initMenu(employeeId: any) {
+    let url = '/employees/' + employeeId + '/vacations';
+    this.items = [
+      { label: 'К сотруднику', icon: '', routerLink: url }
+    ];
+  }
+
+  goToEntry(url: string) {
+    this.router.navigate([url]);
   }
 
 }
