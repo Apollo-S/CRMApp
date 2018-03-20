6b320436285f332692d110de07300c23ae5e02d6
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-03-20 23:06:51 +0200

    BE: colors for logging, fixes in DocumentController
    + added constants for logging in BaseController;
    * new method in DocumentController, improved logging;

--- crmapp/src/main/java/crmapp/app/controllers/DocumentController.java
+++ crmapp/src/main/java/crmapp/app/controllers/DocumentController.java
@@ -34,44 +34,73 @@ public class DocumentController extends BaseController {
 
 	@GetMapping(value = "/documents", headers = HEADER_JSON)
 	public ResponseEntity<List<Document>> getAllDocuments() {
+		logger.info(LOG_ENTER_METHOD + "getAllDocuments()" + LOG_CLOSE);
 		List<Document> documents = documentRepository.findAll();
 		if (documents.size() == 0) {
+			logger.info(LOG_ERROR + "Documents were not found" + LOG_CLOSE);
 			return new ResponseEntity<List<Document>>(HttpStatus.NO_CONTENT);
 		}
+		logger.info(LOG_TEXT + "Count of documents: " + documents.size() + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "getAllDocuments()" + LOG_CLOSE);
 		return new ResponseEntity<List<Document>>(documents, HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/agreements/{agreementId}/documents", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Document>> getAllDocumentsByAgreementId(@PathVariable("agreementId") int agreementId) {
-		logger.info("<==/////////// Entering to the getAllAgreementsByClientId() method ... ///////////==>");
+		logger.info(LOG_ENTER_METHOD + "getAllDocumentsByAgreementId()" + LOG_CLOSE);
 		List<Document> documents = documentRepository.findAllDocumentsByAgreementId(agreementId);
-		logger.info("<==/////////// Printing agreements: " + documents + " ///////////==>");
 		if (documents.size() == 0) {
+			logger.info(LOG_ERROR + "Documents were not found" + LOG_CLOSE);
 			return new ResponseEntity<List<Document>>(HttpStatus.NO_CONTENT);
 		}
+		logger.info(LOG_TEXT + "Count of documents equals " + documents.size() + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "getAllDocumentsByAgreementId()" + LOG_CLOSE);
 		return new ResponseEntity<List<Document>>(documents, HttpStatus.OK);
 	}
 
+	@GetMapping(value = "/documents/{id}", headers = HEADER_JSON)
+	public ResponseEntity<Document> getDocumentById(@PathVariable(PARAM_ID) int id) {
+		logger.info(LOG_ENTER_METHOD + "getDocumentById()" + LOG_CLOSE);
+		Document document = documentRepository.findOne(id);
+		if (document == null) {
+			logger.info(LOG_ERROR + "Document with ID=" + id + "wasn't found" + LOG_CLOSE);
+			return new ResponseEntity<Document>(document, HttpStatus.NOT_FOUND);
+		}
+		logger.info(LOG_TEXT + "Document with ID=" + id + " was found: " + document + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "getDocumentById()" + LOG_CLOSE);
+		return new ResponseEntity<Document>(document, HttpStatus.OK);
+	}
+
 	@PostMapping(value = "/documents", headers = HEADER_JSON)
-	public ResponseEntity<Void> addDocument(@RequestBody Document document) {
+	public ResponseEntity<Document> addDocument(@RequestBody Document document) {
+		logger.info(LOG_ENTER_METHOD + "addDocument()" + LOG_CLOSE);
+		document.setVersion(0);
 		document = documentRepository.save(document);
+		logger.info(LOG_TEXT + "Document added with ID=" + document.getId() + LOG_CLOSE);
 		HttpHeaders header = new HttpHeaders();
-		return new ResponseEntity<Void>(header, HttpStatus.CREATED);
+		logger.info(LOG_OUT_OF_METHOD + "addDocument()" + LOG_CLOSE);
+		return new ResponseEntity<Document>(document, header, HttpStatus.CREATED);
 	}
 
 	@PutMapping(value = "/documents/{id}", headers = HEADER_JSON)
-	public ResponseEntity<Void> updateDocument(@PathVariable(PARAM_ID) int id, @RequestBody Document document) {
+	public ResponseEntity<Document> updateDocument(@PathVariable(PARAM_ID) int id, @RequestBody Document document) {
+		logger.info(LOG_ENTER_METHOD + "updateDocument()" + LOG_CLOSE);
 		document.setId(id);
 		document.setVersion(documentRepository.getOne(id).getVersion());
 		document = documentRepository.save(document);
+		logger.info(LOG_TEXT + "Document with ID=" + id + " was updated: " + document + LOG_CLOSE);
 		HttpHeaders header = new HttpHeaders();
-		return new ResponseEntity<Void>(header, HttpStatus.OK);
+		logger.info(LOG_OUT_OF_METHOD + "updateDocument()" + LOG_CLOSE);
+		return new ResponseEntity<Document>(document, header, HttpStatus.OK);
 	}
 
 	@DeleteMapping(value = "/documents/{id}", headers = HEADER_JSON)
 	public ResponseEntity<Void> deleteDocument(@PathVariable(PARAM_ID) int id, @RequestBody Document document) {
+		logger.info(LOG_ENTER_METHOD + "deleteDocument()" + LOG_CLOSE);
 		documentRepository.delete(document);
+		logger.info(LOG_TEXT + "Document with ID=" + id + " was deleted: " + document + LOG_CLOSE);
 		HttpHeaders header = new HttpHeaders();
+		logger.info(LOG_OUT_OF_METHOD + "updateDocument()" + LOG_CLOSE);
 		return new ResponseEntity<Void>(header, HttpStatus.NO_CONTENT);
 	}
 
