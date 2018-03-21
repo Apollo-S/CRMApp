187330e00e83d4a2277ae4c6d6e73260913e4d4d
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-03-21 13:20:43 +0200

    BE: improved logging in PersonController

--- crmapp/src/main/java/crmapp/app/controllers/PersonController.java
+++ crmapp/src/main/java/crmapp/app/controllers/PersonController.java
@@ -33,48 +33,60 @@ public class PersonController extends BaseController {
 
 	@GetMapping(value = "", headers = HEADER_JSON)
 	public ResponseEntity<List<Person>> getAllPersons() {
+		logger.info(LOG_ENTER_METHOD + "getAllPersons()" + LOG_CLOSE);
 		List<Person> persons = personRepository.findAll();
 		if (persons.size() == 0) {
+			logger.info(LOG_ERROR + "No Person was found" + LOG_CLOSE);
 			return new ResponseEntity<List<Person>>(HttpStatus.NO_CONTENT);
 		}
+		logger.info(LOG_TEXT + "Count of persons: " + persons.size() + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "getAllPersons()" + LOG_CLOSE);
 		return new ResponseEntity<List<Person>>(persons, HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<Person> getPersonById(@PathVariable(PARAM_ID) int id) {
+		logger.info(LOG_ENTER_METHOD + "getPersonById()" + LOG_CLOSE);
 		Person person = personRepository.findOne(id);
 		if (person == null) {
-			logger.info("<==/////////// Person not found with ID=" + id + "///////////==>");
+			logger.info(LOG_ERROR + "Person wasn't found with ID=" + id + LOG_CLOSE);
 			return new ResponseEntity<Person>(person, HttpStatus.NOT_FOUND);
 		}
-		logger.info("<==/////////// Person with ID=" + person.getId() + " was found ///////////==>");
+		logger.info(LOG_TEXT + "Person with ID=" + id + " was found: " + person + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "getPersonById()" + LOG_CLOSE);
 		return new ResponseEntity<Person>(person, HttpStatus.OK);
 	}
 
 	@PostMapping(value = "", headers = HEADER_JSON)
 	public ResponseEntity<Person> addPerson(@RequestBody Person person) {
+		logger.info(LOG_ENTER_METHOD + "addPerson()" + LOG_CLOSE);
 		person.setVersion(0);
 		person = personRepository.save(person);
-		logger.info("<==/////////// Added person, ID=" + person.getId() + "///////////==>");
+		logger.info(LOG_TEXT + "Person added  with ID=" + person.getId() + LOG_CLOSE);
 		HttpHeaders header = new HttpHeaders();
+		logger.info(LOG_OUT_OF_METHOD + "addPerson()" + LOG_CLOSE);
 		return new ResponseEntity<Person>(person, header, HttpStatus.CREATED);
 	}
 
 	@PutMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<Person> updatePerson(@PathVariable(PARAM_ID) int id, @RequestBody Person person) {
+		logger.info(LOG_ENTER_METHOD + "updatePerson()" + LOG_CLOSE);
 		person.setId(id);
 		person.setVersion(personRepository.getOne(id).getVersion());
 		person = personRepository.save(person);
-		logger.info("<==/////////// Person with ID=" + person.getId() + " was updated ///////////==>");
+		logger.info(LOG_TEXT + "Person with ID=" + id + " was updated" + LOG_CLOSE);
 		HttpHeaders header = new HttpHeaders();
+		logger.info(LOG_OUT_OF_METHOD + "updatePerson()" + LOG_CLOSE);
 		return new ResponseEntity<Person>(person, header, HttpStatus.OK);
 	}
 
 	@DeleteMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<Void> deletePerson(@PathVariable(PARAM_ID) int id) {
+		logger.info(LOG_ENTER_METHOD + "deletePerson()" + LOG_CLOSE);
 		personRepository.delete(id);
-		logger.info("<==/////////// Person with ID=" + id + " was deleted ///////////==>");
+		logger.info(LOG_TEXT + "Person with ID=" + id + " was deleted" + LOG_CLOSE);
 		HttpHeaders header = new HttpHeaders();
+		logger.info(LOG_OUT_OF_METHOD + "deletePerson()" + LOG_CLOSE);
 		return new ResponseEntity<Void>(header, HttpStatus.NO_CONTENT);
 	}
 
