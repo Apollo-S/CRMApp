78225bbec56a5447f624f33887def34f3c86c111
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-03-21 13:58:42 +0200

    FE: added properties

--- crmapp/frontend/src/app/models/Document.ts
+++ crmapp/frontend/src/app/models/Document.ts
@@ -4,12 +4,15 @@ import { DocumentStatus } from "./DocumentStatus";
 export class Document {
     public id?: number;
     public docType?: DocumentType;
+    public docTypeTitle?: string;
     public number?: number;
     public amount?: Number;
     public dated?: string;
     public paymentDate?: string;
     public comment?: string;
-    public docStatus?: DocumentStatus;
+    public status?: DocumentStatus;
+    public docStatus?: string;
     public agreementId?: number;
     public agreementNumber?: string;
+    public url?: string;
 }
