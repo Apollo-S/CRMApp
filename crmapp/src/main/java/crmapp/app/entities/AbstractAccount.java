2def378c370c937e9f27c6181aa25d0239e273c9
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-03-22 13:39:57 +0200

    BE: added abstract classes for account and address

---
+++ crmapp/src/main/java/crmapp/app/entities/AbstractAccount.java
@@ -0,0 +1,48 @@
+package crmapp.app.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@MappedSuperclass
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public abstract class AbstractAccount extends UrlBaseEntity {
+
+	@Column(name = "presentation", length = 255)
+	private String presentation;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "date_start")
+	private Date dateStart;
+
+	public String getPresentation() {
+		return presentation;
+	}
+
+	public void setPresentation(String presentation) {
+		this.presentation = presentation;
+	}
+
+	public Date getDateStart() {
+		return dateStart;
+	}
+
+	public void setDateStart(Date dateStart) {
+		this.dateStart = dateStart;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(super.toString()).append(", ");
+		builder.append("presentation=" + presentation).append(", ");
+		builder.append("dateStart=" + dateStart);
+		return builder.toString();
+	}
+
+}
