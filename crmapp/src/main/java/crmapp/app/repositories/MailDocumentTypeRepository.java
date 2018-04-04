eac4c229fe43796ff3a7fdf89f6a5d5813688d50
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-04-04 23:32:42 +0300

    BE: added classes for mail doc type (entity, repository, controller)

---
+++ crmapp/src/main/java/crmapp/app/repositories/MailDocumentTypeRepository.java
@@ -0,0 +1,7 @@
+package crmapp.app.repositories;
+
+import crmapp.app.entities.MailDocumentType;
+
+public interface MailDocumentTypeRepository extends BaseRepository<MailDocumentType, Integer> {
+
+}
