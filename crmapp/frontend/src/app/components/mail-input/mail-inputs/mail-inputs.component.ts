52df8c748b52dfb22b2e873d250e2dde6eb244d4
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-04-05 12:26:39 +0300

    FE: added mail inputs component (view all)

---
+++ crmapp/frontend/src/app/components/mail-input/mail-inputs/mail-inputs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-inputs',
+  templateUrl: './mail-inputs.component.html',
+  styleUrls: ['./mail-inputs.component.css']
+})
+export class MailInputsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
