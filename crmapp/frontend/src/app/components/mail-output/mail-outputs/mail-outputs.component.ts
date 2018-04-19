df6334db2ddf5c87e2a47ca5f8bf2e5f45029177
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-04-19 09:46:16 +0300

    FE: first implementation of mail-outputs page

--- crmapp/frontend/src/app/components/mail-output/mail-outputs/mail-outputs.component.ts
+++ crmapp/frontend/src/app/components/mail-output/mail-outputs/mail-outputs.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MailOutputService } from '../../../services/mail-output.service';
+import { MailOutput } from '../../../models/MailOutput';
+import { MenuItem } from 'primeng/api';
+import { MailDocumentTypeService } from '../../../services/mail-document-type.service';
+import { MailDocumentType } from '../../../models/MailDocumentType';
+import { UtilService } from '../../../services/util.service';
 
 @Component({
   selector: 'app-mail-outputs',
@@ -6,10 +12,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mail-outputs.component.css']
 })
 export class MailOutputsComponent implements OnInit {
+  columns: any[] = [];
+  items: MenuItem[] = [];
+  outputs: MailOutput[] = [];
+  docTypes: MailDocumentType[] = [];
+  mailOutput: MailOutput = {};
+  selectedMailOutput: MailOutput = {};
+  newMailOutput: boolean = false;
+  displayDialog: boolean = false;
+  years: string;
+  ru: any;
 
-  constructor() { }
+  constructor(private outputService: MailOutputService,
+              private docTypeService: MailDocumentTypeService,
+              private utilService: UtilService) { }
 
   ngOnInit() {
+    this.initColumns();
+    this.getOutputs();
+    this.initMenu();
+    this.getDocTypes();
+    this.ru = this.utilService.getCalendarLocalSet();
+    this.years = this.utilService.getCalendarYears(5);
+  }
+
+  private getOutputs() {
+    this.outputService.getMailOutputs()
+      .subscribe(
+        outputs => this.outputs = outputs
+      );
+  }
+
+  private getDocTypes() {
+    this.docTypeService.getMailDocumentTypes()
+      .subscribe(
+        docTypes => this.docTypes = docTypes
+      );
+  }
+
+  private initColumns() {
+    this.columns = [
+      { field: 'number', header: 'Номер' },
+      { field: 'docTypeTitle', header: 'Тип' },
+      { field: 'receiver', header: 'Получатель' },
+      { field: 'dated', header: 'Дата' },
+    ];
+  }
+
+  private initMenu() {
+    this.items = [];
+  }
+
+  onRowSelect(event) {
+    this.newMailOutput = false;
+    this.mailOutput = this.cloneMailOutput(event.data);
+    this.displayDialog = true;
+  } 
+
+  cloneMailOutput(mailOut: MailOutput): MailOutput {
+    let mailOutput = {};
+    for (let prop in mailOut) {
+        mailOutput[prop] = mailOut[prop];
+    }
+    return mailOutput;
   }
 
 }
