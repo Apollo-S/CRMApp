ccceaccaadb27c293ec72e381c8ead0ab8da94b4
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-04-26 16:24:06 +0300

    FE: modified header in Dialog according to its instance (edit or add)

--- crmapp/frontend/src/app/components/mail-output/mail-outputs/mail-outputs.component.ts
+++ crmapp/frontend/src/app/components/mail-output/mail-outputs/mail-outputs.component.ts
@@ -5,6 +5,7 @@ import { MenuItem } from 'primeng/api';
 import { MailDocumentTypeService } from '../../../services/mail-document-type.service';
 import { MailDocumentType } from '../../../models/MailDocumentType';
 import { UtilService } from '../../../services/util.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-mail-outputs',
@@ -23,6 +24,7 @@ export class MailOutputsComponent implements OnInit {
   years: string;
   ru: any;
   disabledMailOutputNumber: boolean = false;
+  headerText: string = "";
 
   constructor(private outputService: MailOutputService,
               private docTypeService: MailDocumentTypeService,
@@ -33,8 +35,12 @@ export class MailOutputsComponent implements OnInit {
     this.getOutputs();
     this.initMenu();
     this.getDocTypes();
+    this.setCalendarSettings(5);
+  }
+
+  private setCalendarSettings(years: number) {
     this.ru = this.utilService.getCalendarLocalSet();
-    this.years = this.utilService.getCalendarYears(5);
+    this.years = this.utilService.getCalendarYears(years);
   }
 
   private getOutputs() {
@@ -70,6 +76,8 @@ export class MailOutputsComponent implements OnInit {
     this.disabledMailOutputNumber = false;
     this.mailOutput = this.cloneMailOutput(event.data);
     this.mailOutput.dated = new Date(this.mailOutput.dated);
+    this.headerText = "Исх. документ № " + this.mailOutput.number + " от " + 
+      (this.mailOutput.dated == null ? '-' : new DatePipe('en-US').transform(this.mailOutput.dated, 'dd.MM.y'));
     this.displayDialog = true;
   } 
 
@@ -82,8 +90,10 @@ export class MailOutputsComponent implements OnInit {
   }
 
   showDialogToAdd() {
+    this.headerText = "Новый исх. документ";
     this.newMailOutput = true;
     this.mailOutput = {};
+    this.mailOutput.dated = new Date();
     this.displayDialog = true;
   }
 
