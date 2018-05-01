8bfd824b779b6680aedebe1d6d17765b0611c59f
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-05-01 23:43:42 +0300

    BE: post, update, delete methods for OurCompany

--- crmapp/src/main/java/crmapp/app/controllers/OurCompanyController.java
+++ crmapp/src/main/java/crmapp/app/controllers/OurCompanyController.java
@@ -5,17 +5,20 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import crmapp.app.entities.Client;
 import crmapp.app.entities.OurCompany;
-import crmapp.app.repositories.ClientRepository;
 import crmapp.app.repositories.OurCompanyRepository;
 
 @RestController
@@ -52,6 +55,41 @@ public class OurCompanyController extends BaseController {
 		logger.info(LOG_TEXT + "OurCompany with ID=" + id + " was found: " + company + LOG_CLOSE);
 		logger.info(LOG_OUT_OF_METHOD + "getOurCompanyById()" + LOG_CLOSE);
 		return new ResponseEntity<OurCompany>(company, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "", headers = HEADER_JSON)
+	public ResponseEntity<OurCompany> addOurCompany(@RequestBody OurCompany company) {
+		logger.info(LOG_ENTER_METHOD + "addOurCompany()" + LOG_CLOSE);
+		company.setVersion(0);
+		company = companyRepository.save(company);
+		logger.info(LOG_TEXT + "OurCompany added with ID=" + company.getId() + LOG_CLOSE);
+		HttpHeaders header = new HttpHeaders();
+		logger.info(LOG_OUT_OF_METHOD + "addOurCompany()" + LOG_CLOSE);
+		return new ResponseEntity<OurCompany>(company, header, HttpStatus.CREATED);
+	}
+
+	@PutMapping(value = "/{id}", headers = HEADER_JSON)
+	public ResponseEntity<OurCompany> updateOurCompany(@PathVariable(PARAM_ID) int id,
+			@RequestBody OurCompany company) {
+		logger.info(LOG_ENTER_METHOD + "updateOurCompany()" + LOG_CLOSE);
+		company.setId(id);
+		int actualVersionNumber = companyRepository.getOne(id).getVersion();
+		company.setVersion(actualVersionNumber);
+		company = companyRepository.save(company);
+		logger.info(LOG_TEXT + "OurCompany with ID=" + id + " was updated: " + company + LOG_CLOSE);
+		HttpHeaders header = new HttpHeaders();
+		logger.info(LOG_OUT_OF_METHOD + "updateOurCompany()" + LOG_CLOSE);
+		return new ResponseEntity<OurCompany>(company, header, HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "/{id}", headers = HEADER_JSON)
+	public ResponseEntity<Void> deleteOurCompany(@PathVariable(PARAM_ID) int id) {
+		logger.info(LOG_ENTER_METHOD + "deleteOurCompany()" + LOG_CLOSE);
+		companyRepository.delete(id);
+		logger.info(LOG_TEXT + "OurCompany with ID=" + id + " was deleted" + LOG_CLOSE);
+		HttpHeaders header = new HttpHeaders();
+		logger.info(LOG_OUT_OF_METHOD + "deleteOurCompany()" + LOG_CLOSE);
+		return new ResponseEntity<Void>(header, HttpStatus.NO_CONTENT);
 	}
 
 }
