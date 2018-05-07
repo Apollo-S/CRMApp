92610ff894d18561e829e5a16539244c26b5b3cf
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-05-07 23:42:41 +0300

    BE: some code refactoring in client controller

--- crmapp/src/main/java/crmapp/app/controllers/ClientController.java
+++ crmapp/src/main/java/crmapp/app/controllers/ClientController.java
@@ -1,6 +1,7 @@
 package crmapp.app.controllers;
 
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import crmapp.app.entities.Client;
 import crmapp.app.repositories.ClientRepository;
 
