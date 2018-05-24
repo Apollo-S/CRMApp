be1505fb7198b140a686eb365bd4c4b7c29a6632
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-05-24 23:21:12 +0300

    FE: some changes in editing agreement

--- crmapp/frontend/src/app/components/agreement/edit-agreement/edit-agreement.component.ts
+++ crmapp/frontend/src/app/components/agreement/edit-agreement/edit-agreement.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ClientAgreement } from '../../../models/ClientAgreement';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Message } from 'primeng/api';
+import { Subscription } from 'rxjs';
+import { AgreementService } from '../../../services/agreement.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-agreement',
   templateUrl: './edit-agreement.component.html',
   styleUrls: ['./edit-agreement.component.css']
 })
-export class EditAgreementComponent implements OnInit {
+export class EditAgreementComponent implements OnInit , OnDestroy {
+  private _propertySubscribtion: Subscription;
+  msgs: Message[] = [];
+  userform: FormGroup;
+  agreement: ClientAgreement = {};
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private service: AgreementService,
+              private router: Router) { }
 
   ngOnInit() {
+    this._propertySubscribtion = this.service.property$
+      .subscribe(
+        p => this.agreement = p
+      );
+      this.userform = this.fb.group(
+        {
+          'title': new FormControl('', Validators.compose(
+            [
+              Validators.required, 
+              Validators.minLength(2)
+            ])),
+          'alias': new FormControl('', Validators.compose(
+            [
+              Validators.required, 
+              Validators.minLength(2)
+            ])),
+          'edrpou': new FormControl('', Validators.compose(
+            [
+              Validators.required, 
+              Validators.minLength(6),
+              Validators.maxLength(14)
+            ])),
+          'inn': new FormControl(''),
+          'vatCertificate': new FormControl('')
+        });
+  }
+
+  ngOnDestroy() {
+    this._propertySubscribtion.unsubscribe();
   }
 
 }
