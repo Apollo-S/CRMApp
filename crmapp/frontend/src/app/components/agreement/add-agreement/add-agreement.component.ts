d2bf2f8e14386186d4f029c54575599f4e773b21
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-06-07 16:58:53 +0300

    FE: implemented form for adding agreement

--- crmapp/frontend/src/app/components/agreement/add-agreement/add-agreement.component.ts
+++ crmapp/frontend/src/app/components/agreement/add-agreement/add-agreement.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem, Message } from 'primeng/api';
+import { Client } from '../../../models/Client';
+import { ClientAgreement } from '../../../models/ClientAgreement';
+import { UtilService } from '../../../services/util.service';
+import { ClientService } from '../../../services/client.service';
+import { AgreementService } from '../../../services/agreement.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-agreement',
@@ -6,10 +13,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-agreement.component.css']
 })
 export class AddAgreementComponent implements OnInit {
+  tabs: MenuItem[];
+  msgs: Message[] = [];
+  clients: Client[] = [];
+  agreement: ClientAgreement = {};
+  years: string;
+  ru: any;
 
-  constructor() { }
+  constructor(private utilService: UtilService,
+              private clientService: ClientService,
+              private agreementService: AgreementService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.initTabs();
+    this.getClients();
+    this.initCalendarSettings();
+  }
+
+  private initTabs(): any {
+    this.tabs = [
+      {label: 'Основные данные', icon: 'fa-address-card-o', disabled: true },
+      {label: 'Связанные документы', icon: 'fa-file-text-o', disabled: true}
+    ];
+  }
+
+  private getClients() {
+    this.clientService.getClients()
+      .subscribe(
+        clients => this.clients = clients
+      );
+  }
+
+  private initCalendarSettings() {
+    this.ru = this.utilService.getCalendarLocalSet();
+    this.years = this.utilService.getCalendarYears(5);
   }
 
 }
