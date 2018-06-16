cd1b9ca5360b0bcb9151f7786d36c1178026c7e2
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-06-16 21:44:00 +0300

    FE: added validators to edit agreement

--- crmapp/frontend/src/app/components/agreement/edit-agreement/edit-agreement.component.ts
+++ crmapp/frontend/src/app/components/agreement/edit-agreement/edit-agreement.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { ClientService } from '../../../services/client.service';
 import { UtilService } from '../../../services/util.service';
 import { Client } from '../../../models/Client';
+import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-agreement',
@@ -18,12 +19,13 @@ export class EditAgreementComponent implements OnInit , OnDestroy {
   msgs: Message[] = [];
   clients: Client[] = [];
   agreement: ClientAgreement = {};
+  userform: FormGroup;
   years: string;
   ru: any;
 
-  constructor(private utilService: UtilService,
-              private clientService: ClientService,
+  constructor(private clientService: ClientService,
               private agreementService: AgreementService,
+              private fb: FormBuilder,
               private router: Router) { }
 
   ngOnInit() {
@@ -38,12 +40,34 @@ export class EditAgreementComponent implements OnInit , OnDestroy {
             );
         }
       );
+    this.initUserForm();
     this.getClients();
     this.initCalendarSettings();
   }
 
+  onSubmit() {
+    this.update();
+    this.goBackToAgreement(1500);
+  }
+
   ngOnDestroy() {
     this._propertySubscribtion.unsubscribe();
+  }
+
+  private initUserForm() {
+    this.userform = this.fb.group({
+      'number': new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)
+      ])),
+      'client': new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      'dateStart': new FormControl('', Validators.compose([
+        Validators.required,
+      ])),
+      'comment': new FormControl('')
+    });
   }
 
   private getClients() {
@@ -54,8 +78,26 @@ export class EditAgreementComponent implements OnInit , OnDestroy {
   }
 
   private initCalendarSettings() {
-    this.ru = this.utilService.getCalendarLocalSet();
-    this.years = this.utilService.getCalendarYears(5);
+    this.ru = UtilService.getCalendarLocalSet();
+    this.years = UtilService.getCalendarYears(5);
   }
+
+  private update(): void {
+    this.agreementService.updateAgreement(this.agreement)
+      .subscribe(
+        response => {
+          this.agreement = response;
+          let msg = 'Договор №' + this.agreement.number +  ' успешно изменен (ID=' + response.id + ')';
+          this.msgs = [{severity:'success', summary:'Успешно!', detail: msg}];
+        }
+      );
+  }
+
+  private goBackToAgreement(timeMillis: number) {
+    setTimeout(
+      (router) => {
+        this.router.navigate([this.agreement.url]);
+      }, timeMillis);
+  } 
 
 }
