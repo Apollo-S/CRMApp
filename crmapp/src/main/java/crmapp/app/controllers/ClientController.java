ac4773a62a82c6dcd93d250f4c6849b4d173d08f
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-07-05 16:15:19 +0300

    BE: moved business logic to client service layer

--- crmapp/src/main/java/crmapp/app/controllers/ClientController.java
+++ crmapp/src/main/java/crmapp/app/controllers/ClientController.java
@@ -19,22 +19,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import crmapp.app.entities.Client;
-import crmapp.app.repositories.ClientRepository;
+import crmapp.app.services.ClientService;
 
 @RestController
-@Transactional
 @RequestMapping(value = "/api/clients")
 public class ClientController extends BaseController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
-	
+
 	@Autowired
-	private ClientRepository clientRepository;
+	private ClientService service;
 
 	@GetMapping(value = "", headers = HEADER_JSON)
 	public ResponseEntity<List<Client>> getAllClients() {
 		logger.info(LOG_ENTER_METHOD + "getAllClients()" + LOG_CLOSE);
-		List<Client> clients = clientRepository.findAll();
+		List<Client> clients = service.getAll();
 		if (clients.size() == 0) {
 			logger.info(LOG_ERROR + "Clients were not found" + LOG_CLOSE);
 			return new ResponseEntity<List<Client>>(HttpStatus.NO_CONTENT);
@@ -47,7 +46,7 @@ public class ClientController extends BaseController {
 	@GetMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<Client> getClientById(@PathVariable(PARAM_ID) int id) {
 		logger.info(LOG_ENTER_METHOD + "getClientById()" + LOG_CLOSE);
-		Client client = clientRepository.findOne(id);
+		Client client = service.getById(id);
 		if (client == null) {
 			logger.info(LOG_ERROR + "Client with ID=" + id + "wasn't found" + LOG_CLOSE);
 			return new ResponseEntity<Client>(client, HttpStatus.NOT_FOUND);
@@ -60,20 +59,16 @@ public class ClientController extends BaseController {
 	@PostMapping(value = "", headers = HEADER_JSON)
 	public ResponseEntity<Client> addClient(@RequestBody Client client) {
 		logger.info(LOG_ENTER_METHOD + "addClient()" + LOG_CLOSE);
-		client.setVersion(0);
-		client = clientRepository.save(client);
-		logger.info(LOG_TEXT + "Client added with ID=" + client.getId() + LOG_CLOSE);
+		Client savedClient = service.save(client);
+		logger.info(LOG_TEXT + "Client added with ID=" + savedClient.getId() + LOG_CLOSE);
 		logger.info(LOG_OUT_OF_METHOD + "addClient()" + LOG_CLOSE);
-		return new ResponseEntity<Client>(client, new HttpHeaders(), HttpStatus.CREATED);
+		return new ResponseEntity<Client>(savedClient, new HttpHeaders(), HttpStatus.CREATED);
 	}
 
 	@PutMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<Client> updateClient(@PathVariable(PARAM_ID) int id, @RequestBody Client client) {
 		logger.info(LOG_ENTER_METHOD + "updateClient()" + LOG_CLOSE);
-		client.setId(id);
-		int actualVersionNumber = clientRepository.getOne(id).getVersion();
-		client.setVersion(actualVersionNumber);
-		client = clientRepository.save(client);
+		client = service.update(id, client);
 		logger.info(LOG_TEXT + "Client with ID=" + id + " was updated: " + client + LOG_CLOSE);
 		logger.info(LOG_OUT_OF_METHOD + "updateClient()" + LOG_CLOSE);
 		return new ResponseEntity<Client>(client, new HttpHeaders(), HttpStatus.OK);
@@ -82,7 +77,7 @@ public class ClientController extends BaseController {
 	@DeleteMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<Void> deleteClient(@PathVariable(PARAM_ID) int id) {
 		logger.info(LOG_ENTER_METHOD + "deleteClient()" + LOG_CLOSE);
-		clientRepository.delete(id);
+		service.delete(id);
 		logger.info(LOG_TEXT + "Client with ID=" + id + " was deleted" + LOG_CLOSE);
 		logger.info(LOG_OUT_OF_METHOD + "deleteClient()" + LOG_CLOSE);
 		return new ResponseEntity<Void>(new HttpHeaders(), HttpStatus.NO_CONTENT);
