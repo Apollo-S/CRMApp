03a6f99b10983576cce92404743e763dc4e0e634
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-07-14 23:32:57 +0300

    BE: added vacation service

---
+++ crmapp/src/main/java/crmapp/app/services/VacationService.java
@@ -0,0 +1,24 @@
+package crmapp.app.services;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import crmapp.app.entities.Vacation;
+import crmapp.app.repositories.VacationRepository;
+
+@Service
+public class VacationService extends AbstractService<Vacation, VacationRepository> {
+
+	private static final Logger logger = LoggerFactory.getLogger(VacationService.class);
+
+	public List<Vacation> getAllByEmployeeId(int id) {
+		logger.info("OK: VacationService.getAllByEmployeeId()");
+		List<Vacation> vacations = repository.findAllVacationsByEmployeeId(id);
+		logger.info("OK: Count of vacations equals " + vacations.size());
+		return vacations;
+	}
+
+}
