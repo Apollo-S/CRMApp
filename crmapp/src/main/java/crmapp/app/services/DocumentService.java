03c46bede7f60707f3be8047bb2d1295fad93980
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-07-16 22:20:06 +0300

    BE: document service extends abstract service, refactoring

--- crmapp/src/main/java/crmapp/app/services/DocumentService.java
+++ crmapp/src/main/java/crmapp/app/services/DocumentService.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import static org.springframework.data.domain.Sort.Direction.fromString;
 import org.springframework.stereotype.Service;
@@ -20,12 +19,9 @@ import crmapp.app.repositories.DocumentTypeRepository;
 
 @Service
 @Transactional
-public class DocumentService implements BaseService<Document> {
+public class DocumentService extends AbstractService<Document, DocumentRepository> {
 
 	private static final Logger logger = LoggerFactory.getLogger(DocumentService.class);
-
-	@Autowired
-	private DocumentRepository docRepository;
 
 	@Autowired
 	private ClientRepository clientRepository;
@@ -36,40 +32,15 @@ public class DocumentService implements BaseService<Document> {
 	@Autowired
 	private DocumentStatusRepository docStatusRepository;
 
-	@Override
-	public List<Document> getAll() {
-		logger.info("OK: DocumentService.getAll()");
-		List<Document> documents = ((JpaRepository<Document,Integer>) docRepository).findAll();
-		logger.info("OK: Documents in quantity=" + documents.size() + " found");
-		return documents;
-	}
-
-	@Override
-	public Document getById(int id) {
-		logger.info("OK: DocumentService.getById()");
-		Document document = docRepository.findOne(id);
-		logger.info("OK: Document with ID="+ id + " found");
-		return document;
-	}
-
 	public List<Document> getAllByAgreementId(int id) {
 		logger.info("OK: DocumentService.getAllByAgreementId()");
-		List<Document> documents = docRepository.findAllDocumentsByAgreementId(id);
+		List<Document> documents = super.repository.findAllDocumentsByAgreementId(id);
 		logger.info("OK: Count of documents equals " + documents.size());
 		return documents;
 	}
 
-	@Override
-	public Document save(Document document) {
-		logger.info("OK: DocumentService.save()");
-		document.setVersion(0);
-		document = docRepository.save(document);
-		logger.info("OK: Document saved: " + document);
-		return document;
-	}
-
-	public List<Document> getAllByFilterAndSort(List<Integer> docTypes, List<Integer> docStatuses, List<Integer> clients,
-			String sortType, String sortField) {
+	public List<Document> getAllByFilterAndSort(List<Integer> docTypes, List<Integer> docStatuses,
+			List<Integer> clients, String sortType, String sortField) {
 		logger.info("OK: DocumentService.getAllByFilterAndSort()");
 		List<Document> documents;
 		if (docTypes.get(0) == 0 || docTypes.isEmpty()) {
@@ -82,27 +53,9 @@ public class DocumentService implements BaseService<Document> {
 			clients = clientRepository.findAllEntityIds();
 		}
 		Sort sort = new Sort(fromString(sortType), sortField);
-		documents = docRepository.findAllDocumentsByFilterAndSort(docTypes, docStatuses, clients, sort);
+		documents = super.repository.findAllDocumentsByFilterAndSort(docTypes, docStatuses, clients, sort);
 		logger.info("OK: Count of documents equals " + documents.size());
 		return documents;
-	}
-
-	@Override
-	public Document update(int id, Document document) {
-		logger.info("OK: DocumentService.update()");
-		document.setId(id);
-		int actualVersionNumber = docRepository.getOne(id).getVersion();
-		document.setVersion(actualVersionNumber);
-		document = docRepository.save(document);
-		logger.info("OK: Document updated: " + document);
-		return document;
-	}
-
-	@Override
-	public void delete(int id) {
-		logger.info("OK: DocumentService.delete()");
-		docRepository.delete(id);
-		logger.info("OK: Document deleted successfully");
 	}
 
 }
