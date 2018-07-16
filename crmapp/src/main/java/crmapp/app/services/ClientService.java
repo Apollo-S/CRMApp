d53b52ba5611a56b92cb1d45d005f6c0276fc45d
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-07-16 22:17:25 +0300

    BE: @Transactional added in client service

--- crmapp/src/main/java/crmapp/app/services/ClientService.java
+++ crmapp/src/main/java/crmapp/app/services/ClientService.java
@@ -1,10 +1,12 @@
 package crmapp.app.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import crmapp.app.entities.Client;
 import crmapp.app.repositories.ClientRepository;
 
 @Service
+@Transactional
 public class ClientService extends AbstractService<Client, ClientRepository> {
 }
