cd50033091cc8578ee03d37988c06296582134a3
apollo.saifullin@gmail.com <apollo.saifullin@gmail.com>
2018-07-17 09:13:56 +0300

    BE: removed @Transactional from controller

--- crmapp/src/main/java/crmapp/app/controllers/DocumentController.java
+++ crmapp/src/main/java/crmapp/app/controllers/DocumentController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ import crmapp.app.entities.Document;
 import crmapp.app.services.DocumentService;
 
 @RestController
-@Transactional
 @RequestMapping(value = "/api")
 public class DocumentController extends BaseController {
 
