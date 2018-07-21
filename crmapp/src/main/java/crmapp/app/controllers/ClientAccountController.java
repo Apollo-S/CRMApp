619a0fdbced60afdc8d9c85ecc3c0061f2eaab7b
Apollon Saifullin <apollo.saifullin@gmail.com>
2018-07-21 23:48:19 +0300

    BE: replaced repo with service

--- crmapp/src/main/java/crmapp/app/controllers/ClientAccountController.java
+++ crmapp/src/main/java/crmapp/app/controllers/ClientAccountController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,18 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import crmapp.app.entities.Client;
 import crmapp.app.entities.ClientAccount;
-import crmapp.app.repositories.ClientAccountRepository;
 import crmapp.app.repositories.ClientRepository;
+import crmapp.app.services.ClientAccountService;
 
 @RestController
-@Transactional
 @RequestMapping(value = "/api/clients/{clientId}/accounts")
 public class ClientAccountController extends BaseController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ClientAccountController.class);
 
 	@Autowired
-	private ClientAccountRepository accountRepository;
+	private ClientAccountService service;
 	
 	@Autowired
 	private ClientRepository clientRepository;
@@ -53,7 +51,7 @@ public class ClientAccountController extends BaseController {
 	@GetMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<ClientAccount> getClientAccountById(@PathVariable(PARAM_ID) int id) {
 		logger.info(LOG_ENTER_METHOD + "getClientAccountById()" + LOG_CLOSE);
-		ClientAccount account = accountRepository.findOne(id);
+		ClientAccount account = service.getById(id);
 		if (account == null) {
 			logger.info(LOG_ERROR + "ClientAccount with ID=" + id + "wasn't found" + LOG_CLOSE);
 			return new ResponseEntity<ClientAccount>(account, HttpStatus.NOT_FOUND);
@@ -71,7 +69,7 @@ public class ClientAccountController extends BaseController {
 		logger.info(LOG_TEXT + "Obtained Client with ID = " + client.getId() + LOG_CLOSE);
 		account.setClient(client);
 		account.setVersion(0);
-		account = accountRepository.save(account);
+		account = service.save(account);
 		logger.info(LOG_TEXT + "ClientAccount added with ID=" + account.getId() + LOG_CLOSE);
 		HttpHeaders header = new HttpHeaders();
 		logger.info(LOG_OUT_OF_METHOD + "addClientAccount()" + LOG_CLOSE);
