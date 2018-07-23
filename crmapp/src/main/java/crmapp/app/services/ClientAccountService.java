82d430509bcd43429a642500e2a7103b27901874
Apollon Saifullin <apollo.saifullin@gmail.com>
2018-07-23 23:58:33 +0300

    BE: added 'findAllByClientId' method to the service

--- crmapp/src/main/java/crmapp/app/services/ClientAccountService.java
+++ crmapp/src/main/java/crmapp/app/services/ClientAccountService.java
@@ -1,5 +1,8 @@
 package crmapp.app.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,5 +12,13 @@ import crmapp.app.repositories.ClientAccountRepository;
 @Service
 @Transactional
 public class ClientAccountService extends AbstractService<ClientAccount, ClientAccountRepository> {
+
+	@Autowired
+	private ClientService clientService;
+
+	public List<ClientAccount> findAllByClientId(Integer clientId) {
+		List<ClientAccount> accounts = repository.findAllByClientId(clientId);
+		return accounts;
+	}
 
 }
