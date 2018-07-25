711f5d18b2b8fedf56a3ae032c8b95503c2d9d44
Apollon Saifullin <apollo.saifullin@gmail.com>
2018-07-25 16:07:40 +0300

    BE: added save(overloaded) and updateWithClientId methods

--- crmapp/src/main/java/crmapp/app/services/ClientAccountService.java
+++ crmapp/src/main/java/crmapp/app/services/ClientAccountService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import crmapp.app.entities.Client;
 import crmapp.app.entities.ClientAccount;
 import crmapp.app.repositories.ClientAccountRepository;
 
@@ -19,6 +20,20 @@ public class ClientAccountService extends AbstractService<ClientAccount, ClientA
 	public List<ClientAccount> findAllByClientId(Integer clientId) {
 		List<ClientAccount> accounts = repository.findAllByClientId(clientId);
 		return accounts;
+	}
+
+	public ClientAccount save(int clientId, ClientAccount account) {
+		Client client = clientService.getById(clientId);
+		account.setClient(client);
+		account = this.save(account);
+		return account;
+	}
+
+	public ClientAccount updateWithClientId(int clientId, ClientAccount account) {
+		Client client = clientService.getById(clientId);
+		account.setClient(client);
+		account = this.update(account);
+		return account;
 	}
 
 }
