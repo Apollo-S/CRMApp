8e7c3c52a2f4ef49c9d0e5c08465f061d40521ea
Apollo-S <apollo.saifullin@gmail.com>
2018-12-01 23:29:46 +0200

    added client address service

---
+++ crmapp/src/main/java/crmapp/app/services/ClientAddressService.java
@@ -0,0 +1,35 @@
+package crmapp.app.services;
+
+import crmapp.app.entities.Client;
+import crmapp.app.entities.ClientAddress;
+import crmapp.app.repositories.ClientAddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ClientAddressService extends AbstractService<ClientAddress, ClientAddressRepository> {
+
+	@Autowired
+	private ClientService clientService;
+
+	public List<ClientAddress> findAllByClientId(Integer clientId) {
+		return repository.findAllClientAddressesByClientId(clientId);
+	}
+
+	public ClientAddress save(int clientId, ClientAddress address) {
+		Client client = clientService.getById(clientId);
+		address.setClient(client);
+		return this.save(address);
+	}
+
+	public ClientAddress updateWithClientId(int clientId, ClientAddress address) {
+		Client client = clientService.getById(clientId);
+		address.setClient(client);
+		return this.update(address);
+	}
+
+}
