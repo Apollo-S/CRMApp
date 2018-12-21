26f5b668bf43996ca27125c422d92910168ed900
Apollo-S <apollo.saifullin@gmail.com>
2018-12-21 23:32:13 +0200

    ClientController was rewrote to work with ClientService

--- crmapp/src/main/java/crmapp/app/controllers/ClientDirectorController.java
+++ crmapp/src/main/java/crmapp/app/controllers/ClientDirectorController.java
@@ -2,6 +2,7 @@ package crmapp.app.controllers;
 
 import java.util.List;
 
+import crmapp.app.services.ClientDirectorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,86 +19,70 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import crmapp.app.entities.Client;
 import crmapp.app.entities.ClientDirector;
-import crmapp.app.repositories.ClientDirectorRepository;
-import crmapp.app.repositories.ClientRepository;
 
 @RestController
-@Transactional
 @RequestMapping(value = "/api/clients/{clientId}/directors")
 public class ClientDirectorController extends BaseController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ClientDirectorController.class);
 
 	@Autowired
-	private ClientDirectorRepository directorRepository;
-
-	@Autowired
-	private ClientRepository clientRepository;
+	private ClientDirectorService directorService;
 
 	@GetMapping(value = "", headers = HEADER_JSON)
 	public ResponseEntity<List<ClientDirector>> getAllClientDirectors(@PathVariable("clientId") Integer clientId) {
 		logger.info(LOG_ENTER_METHOD + "getAllClientDirectors()" + LOG_CLOSE);
-		List<ClientDirector> directors = directorRepository.findAllDirectorsByClientId(clientId);
+		List<ClientDirector> directors = directorService.findAllByClientId(clientId);
 		if (directors.size() == 0) {
 			logger.info(LOG_ERROR + "ClientDirectors were not found" + LOG_CLOSE);
-			return new ResponseEntity<List<ClientDirector>>(HttpStatus.NO_CONTENT);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
 		logger.info(LOG_TEXT + "Count of ClientDirectors: " + directors.size() + LOG_CLOSE);
 		logger.info(LOG_OUT_OF_METHOD + "getAllClientDirectors()" + LOG_CLOSE);
-		return new ResponseEntity<List<ClientDirector>>(directors, HttpStatus.OK);
+		return new ResponseEntity<>(directors, HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<ClientDirector> getClientDirectorById(@PathVariable(PARAM_ID) int id) {
 		logger.info(LOG_ENTER_METHOD + "getClientDirectorById()" + LOG_CLOSE);
-		ClientDirector director = directorRepository.findOne(id);
+		ClientDirector director = directorService.findById(id);
 		if (director == null) {
 			logger.info(LOG_ERROR + "ClientDirector with ID=" + id + "wasn't found" + LOG_CLOSE);
-			return new ResponseEntity<ClientDirector>(director, HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		logger.info(LOG_TEXT + "ClientDirector with ID=" + id + " was found: " + director + LOG_CLOSE);
 		logger.info(LOG_OUT_OF_METHOD + "getClientDirectorById()" + LOG_CLOSE);
-		return new ResponseEntity<ClientDirector>(director, HttpStatus.OK);
+		return new ResponseEntity<>(director, HttpStatus.OK);
 	}
 
 	@PostMapping(value = "", headers = HEADER_JSON)
 	public ResponseEntity<ClientDirector> addClientDirector(@PathVariable("clientId") int clientId,
 			@RequestBody ClientDirector director) {
 		logger.info(LOG_ENTER_METHOD + "addClientDirector()" + LOG_CLOSE);
-		Client client = clientRepository.findOne(clientId);
-		logger.info(LOG_TEXT + "Obtained Client with ID = " + client.getId() + LOG_CLOSE);
-		director.setClient(client);
-		director.setVersion(0);
-		director = directorRepository.save(director);
-		logger.info(LOG_TEXT + "ClientDirector added with ID=" + director.getId() + LOG_CLOSE);
+		ClientDirector savedDirector = directorService.save(clientId, director);
+		logger.info(LOG_TEXT + "ClientDirector added with ID=" + savedDirector.getId() + LOG_CLOSE);
 		logger.info(LOG_OUT_OF_METHOD + "addClientDirector()" + LOG_CLOSE);
-		return new ResponseEntity<ClientDirector>(director, new HttpHeaders(), HttpStatus.CREATED);
+		return new ResponseEntity<>(savedDirector, new HttpHeaders(), HttpStatus.CREATED);
 	}
 
 	@PutMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<ClientDirector> updateClientDirector(@PathVariable("clientId") int clientId, 
 			@RequestBody ClientDirector director) {
 		logger.info(LOG_ENTER_METHOD + "updateClientDirector()" + LOG_CLOSE);
-		Client client = clientRepository.findOne(clientId);
-		director.setClient(client);
-		logger.info(LOG_TEXT + "Client is setted to " + client + LOG_CLOSE);
-		int actualVersionNumber = directorRepository.getOne(director.getId()).getVersion();
-		director.setVersion(actualVersionNumber);
-		director = directorRepository.save(director);
-		logger.info(LOG_TEXT + "ClientDirector was updated: " + director + LOG_CLOSE);
+		ClientDirector updatedDirector = directorService.updateWithClientId(clientId, director);
+		logger.info(LOG_TEXT + "ClientDirector was updated: " + updatedDirector + LOG_CLOSE);
 		logger.info(LOG_OUT_OF_METHOD + "updateClientDirector()" + LOG_CLOSE);
-		return new ResponseEntity<ClientDirector>(director, new HttpHeaders(), HttpStatus.OK);
+		return new ResponseEntity<>(updatedDirector, new HttpHeaders(), HttpStatus.OK);
 	}
 
 	@DeleteMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<Void> deleteClientDirector(@PathVariable(PARAM_ID) int id) {
 		logger.info(LOG_ENTER_METHOD + "deleteClientDirector()" + LOG_CLOSE);
-		directorRepository.delete(id);
+		directorService.delete(id);
 		logger.info(LOG_TEXT + "ClientDirector with ID=" + id + " was deleted" + LOG_CLOSE);
 		logger.info(LOG_OUT_OF_METHOD + "deleteClientDirector()" + LOG_CLOSE);
-		return new ResponseEntity<Void>(new HttpHeaders(), HttpStatus.NO_CONTENT);
+		return new ResponseEntity<>(new HttpHeaders(), HttpStatus.NO_CONTENT);
 	}
 
 }
