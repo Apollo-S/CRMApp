65c3afaf473f3cc9b258d52ac93b0daad2a533ed
Apollo-S <apollo.saifullin@gmail.com>
2018-12-21 22:42:02 +0200

    added service for ClientDirector

---
+++ crmapp/src/main/java/crmapp/app/services/ClientDirectorService.java
@@ -0,0 +1,32 @@
+package crmapp.app.services;
+
+import crmapp.app.entities.ClientDirector;
+import crmapp.app.repositories.ClientDirectorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ClientDirectorService extends AbstractService<ClientDirector, ClientDirectorRepository> {
+
+    @Autowired
+    private ClientService clientService;
+
+    public List<ClientDirector> findAllByClientId(Integer clientId) {
+        return repository.findAllDirectorsByClientId(clientId);
+    }
+
+    public ClientDirector save(int clientId, ClientDirector director) {
+        director.setClient(clientService.findById(clientId));
+        return this.save(director);
+    }
+
+    public ClientDirector updateWithClientId(int clientId, ClientDirector director) {
+        director.setClient(clientService.findById(clientId));
+        return this.update(director);
+    }
+
+}
