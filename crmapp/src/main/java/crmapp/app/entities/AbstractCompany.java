f3a5fb9bc73fe3acf183ed04e8ee6f4a6f6d6247
Apollo-S <apollo.saifullin@gmail.com>
2019-03-08 23:56:46 +0200

    minor fixes

--- crmapp/src/main/java/crmapp/app/entities/AbstractCompany.java
+++ crmapp/src/main/java/crmapp/app/entities/AbstractCompany.java
@@ -6,8 +6,12 @@ import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 @MappedSuperclass
+@Getter
+@Setter
 @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public abstract class AbstractCompany extends BaseEntity {
 
@@ -37,46 +41,6 @@ public abstract class AbstractCompany extends BaseEntity {
 		this.vatCertificate = vatCertificate;
 	}
 
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAlias() {
-		return alias;
-	}
-
-	public void setAlias(String alias) {
-		this.alias = alias;
-	}
-
-	public String getEdrpou() {
-		return edrpou;
-	}
-
-	public void setEdrpou(String edrpou) {
-		this.edrpou = edrpou;
-	}
-
-	public String getInn() {
-		return inn;
-	}
-
-	public void setInn(String inn) {
-		this.inn = inn;
-	}
-
-	public String getVatCertificate() {
-		return vatCertificate;
-	}
-
-	public void setVatCertificate(String vatCertificate) {
-		this.vatCertificate = vatCertificate;
-	}
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(this.getId(), this.getTitle(), this.getEdrpou());
@@ -86,9 +50,7 @@ public abstract class AbstractCompany extends BaseEntity {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
+		if (obj == null || (getClass() != obj.getClass()))
 			return false;
 		AbstractCompany that = (AbstractCompany) obj;
 		return Objects.equals(this.getId(), that.getId()) && 
