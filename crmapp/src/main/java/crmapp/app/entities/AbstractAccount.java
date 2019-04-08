cf59144310f35a0c6df23d0c31afd7b076ff0c54
Apollo-S <apollo.saifullin@gmail.com>
2019-04-08 17:44:12 +0300

    fixed getPresentation()

--- crmapp/src/main/java/crmapp/app/entities/AbstractAccount.java
+++ crmapp/src/main/java/crmapp/app/entities/AbstractAccount.java
@@ -19,62 +19,65 @@ import lombok.Setter;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public abstract class AbstractAccount extends BaseEntity {
 
-	@Column(name = "number", length = 255)
-	private String number;
+    @Column(name = "number", length = 255)
+    private String number;
 
-	@Column(name = "bank_name", length = 200)
-	private String bankName;
+    @Column(name = "bank_name", length = 200)
+    private String bankName;
 
-	@Column(name = "mfo", length = 7)
-	private String mfo;
+    @Column(name = "mfo", length = 7)
+    private String mfo;
 
-	@Temporal(TemporalType.DATE)
-	@Column(name = "date_start")
-	private Date dateStart;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "date_start")
+    private Date dateStart;
 
-	public AbstractAccount() {
-	}
+    public AbstractAccount() {
+    }
 
-	public AbstractAccount(String number, String bankName, String mfo, Date dateStart) {
-		this.number = number;
-		this.bankName = bankName;
-		this.mfo = mfo;
-		this.dateStart = dateStart;
-	}
+    public AbstractAccount(String number, String bankName, String mfo, Date dateStart) {
+        this.number = number;
+        this.bankName = bankName;
+        this.mfo = mfo;
+        this.dateStart = dateStart;
+    }
 
-	@JsonInclude
-	public String getPresentation() {
-		return null;
-	}
+    @JsonInclude
+    public String getPresentation() {
+        StringBuilder accountBuilder = new StringBuilder();
+        accountBuilder.append(getNumber()).append(", ");
+        accountBuilder.append(getBankName()).append(" (МФО ").append(getMfo()).append(")");
+        return accountBuilder.toString();
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(this.getId(), this.getPresentation(), this.getDateStart());
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId(), this.getPresentation(), this.getDateStart());
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		AbstractAccount that = (AbstractAccount) obj;
-		return Objects.equals(this.getId(), that.getId()) && 
-				Objects.equals(this.getPresentation(), that.getPresentation()) && 
-				Objects.equals(this.getDateStart(), that.getDateStart());
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AbstractAccount that = (AbstractAccount) obj;
+        return Objects.equals(this.getId(), that.getId()) &&
+                Objects.equals(this.getPresentation(), that.getPresentation()) &&
+                Objects.equals(this.getDateStart(), that.getDateStart());
+    }
 
-	@Override
-	public String toString() {
-		return new StringBuilder()
-			.append(super.toString()).append(", ")
-			.append("number=" + number).append(", ")
-			.append("bankName=" + bankName).append(", ")
-			.append("mfo=" + mfo).append(", ")
-			.append("dateStart=" + dateStart)
-			.toString();
-	}
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append(super.toString()).append(", ")
+                .append("number=" + number).append(", ")
+                .append("bankName=" + bankName).append(", ")
+                .append("mfo=" + mfo).append(", ")
+                .append("dateStart=" + dateStart)
+                .toString();
+    }
 
 }
