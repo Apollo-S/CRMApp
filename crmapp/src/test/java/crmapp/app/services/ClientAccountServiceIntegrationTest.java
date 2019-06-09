b5879d8b22dd585f5c2320d1110f758e30dc7f79
Apollo-S <apollo.saifullin@gmail.com>
2019-06-09 23:41:47 +0300

    added tests for ClientAccount

---
+++ crmapp/src/test/java/crmapp/app/services/ClientAccountServiceIntegrationTest.java
@@ -0,0 +1,78 @@
+package crmapp.app.services;
+
+import crmapp.app.entities.Client;
+import crmapp.app.entities.ClientAccount;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class ClientAccountServiceIntegrationTest {
+
+    private ClientAccount clientAccount;
+
+    @Autowired
+    private ClientAccountService clientAccountService;
+
+    @Before
+    public void setup() {
+        Date date = new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime();
+        Client client = new Client();
+        client.setId(1);
+
+        clientAccount = new ClientAccount();
+        clientAccount.setNumber("26007017134344");
+        clientAccount.setBankName("АТ \"Сити-Банк\"");
+        clientAccount.setMfo("311009");
+        clientAccount.setDateStart(date);
+        clientAccount.setClient(client);
+    }
+
+    @Test
+    @Transactional
+    public void testAddClientAccount() {
+        ClientAccount savedAccount = clientAccountService.save(this.clientAccount);
+        System.out.println(savedAccount);
+        assertNotNull(savedAccount);
+        assertNotNull(savedAccount.getId());
+        assertEquals("26007017134344", savedAccount.getNumber());
+        assertEquals(new Integer(1), savedAccount.getClient().getId());
+    }
+
+    @Test
+    @Transactional
+    public void testUpdateClientAccount() {
+        ClientAccount savedAccount = clientAccountService.save(this.clientAccount);
+        savedAccount.setBankName("PrivatBank JSPC");
+        savedAccount.setMfo("300711");
+        savedAccount.setNumber("260001313131");
+        savedAccount.setDateStart(new GregorianCalendar(2017, Calendar.MARCH, 25).getTime());
+
+        ClientAccount updatedAccount = clientAccountService.update(savedAccount);
+
+        assertEquals("PrivatBank JSPC", updatedAccount.getBankName());
+        assertEquals("300711", updatedAccount.getMfo());
+        assertEquals("260001313131", updatedAccount.getNumber());
+
+    }
+
+    @Test
+    @Transactional
+    public void testDeleteClientAccount() {
+        ClientAccount savedAccount = clientAccountService.save(this.clientAccount);
+        clientAccountService.delete(savedAccount.getId());
+        assertNull(clientAccountService.findById(savedAccount.getId()));
+    }
+
+}
